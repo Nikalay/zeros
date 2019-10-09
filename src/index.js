b98e133f8196be2a6a69d7ff04e636615d287147
Nikalay <sanskritpro@gmail.com>
2019-10-09 11:43:53 +0300

    add: func one & double fakt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function zeros(expression) {
   // your solution
+  //функция вычисления двойного факториала для четных и нечетных
+  function doubleFakt(n){
+	var result = 1;
+
+	if(expression%2 == 0){
+		for(var j = 2; j <= expression; j=j+2){
+
+			result = result * j;
+		}
+		return result;
+	} else{
+		for(var j = 1; j <= expression; j=j+2){
+
+		result = result * j;
+		}
+		return result;
+		}
+	}
+	//функция вычисления  факториала
+	function oneFakt(expression){
+		var result = 1;
+		for(var j = 1; j <= n; j++){
+			result = result * j;
+		}
+		return result;
+	}
 }
